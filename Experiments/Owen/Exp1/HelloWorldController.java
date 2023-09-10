d052ed5e8d3cf7981e74f62139b4809e0115402c
Owen Parker <oparker@iastate.edu>
2023-09-10 07:01:13 +0000

    Update HelloWorldController.java

--- Experiments/Owen/Exp1/HelloWorldController.java
+++ Experiments/Owen/Exp1/HelloWorldController.java
@@ -1,12 +1,24 @@
 package com.javatpoint.controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
-public class HelloWorldController 
-{
-@RequestMapping("/")
-public String hello() 
-{
-return "Hello my name is Owen Parker";
-}
+public class HelloWorldController {
+    @RequestMapping("/")
+    public String hello() {
+        return "Hello my name is Owen Parker";
+    }
+
+
+    @PostMapping("/people")
+    public @ResponseBody String createPerson(@RequestBody Person person) {
+        System.out.println(person);
+        peopleList.put(person.getFirstName(), person);
+        return "New person " + person.getFirstName() + " Saved";
+    }
+
+    @getMapping("/people/{firstName}")
+    public @ResponseBody Person getPerson(@PathVariable String firstName) {
+        Person p = peopleList.get(firstName);
+        return p;
+    }
 }
