18d3b5cac9caea519cce5c178be9fccb7ef1915a
kyletm <kyletm@pyrite-n2.cs.iastate.edu>
2023-10-06 14:43:53 -0500

    added sha-256 hashing support

--- Backend/tutorials-springboot_unit2_1_onetoone_mysql-springboot_example2/springboot_example/src/main/java/onetoone/Users/User.java
+++ Backend/tutorials-springboot_unit2_1_onetoone_mysql-springboot_example2/springboot_example/src/main/java/onetoone/Users/User.java
@@ -7,6 +7,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 import onetoone.Laptops.Laptop;
 
@@ -55,6 +57,30 @@ public class User {
     }
 
     // =============================== Getters and Setters for each field ================================== //
+
+    public String convert_hex(String[] input) {
+
+        try {
+            // Create a SHA-256 message digest object
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+            // Update the digest with the input string bytes
+            byte[] hash = digest.digest(input.getBytes());
+
+            // Convert the byte array to a hexadecimal string
+            StringBuilder hexString = new StringBuilder();
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) hexString.append('0');
+                hexString.append(hex);
+            }
+
+            System.out.println("SHA-256 Hash: " + hexString.toString());
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+
+        }
+    }
 
     public int getId(){
         return id;
