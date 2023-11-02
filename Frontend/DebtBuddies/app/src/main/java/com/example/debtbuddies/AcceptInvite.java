38ba483786cea642c698341cfcbd645186a32f59
bdykhuis <bdykhuis@iastate.edu>
2023-11-02 18:40:28 -0500

    start of war websockets]

--- Frontend/DebtBuddies/app/src/main/java/com/example/debtbuddies/AcceptInvite.java
+++ Frontend/DebtBuddies/app/src/main/java/com/example/debtbuddies/AcceptInvite.java
@@ -53,7 +53,9 @@ public class AcceptInvite extends AppCompatActivity {
 
     TextView tv_username;
     Button b_accept, b_decline, b_back;
+    String SERVER_URL = " ";
     ImageView icon;
+    String result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -67,15 +69,80 @@ public class AcceptInvite extends AppCompatActivity {
     }
 
     public void onAcceptClicked(View view) {
-
+        result = "accept";
+        postRequest();
     }
     public void onDeclineClicked(View view) {
-
+        result = "decline";
+        postRequest();
     }
     public void onBackClicked(View view) {
         Intent intent = new Intent(this, Party.class);
         startActivity(intent);
     }
+    private void postRequest() {
 
+        // Convert input to JSONObject
+        JSONObject postBody;
+        String temp =
+                "{" +
+                        "\"result\":\"" + result + "\"" +
+                        "}";
+
+        try {
+            postBody = new JSONObject(temp);
+        } catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+        //String postBody = "username:" + username + "password:" + password + "email:" + email;
+
+        try{
+            // etRequest should contain a JSON object string as your POST body
+            // similar to what you would have in POSTMAN-body field
+            // and the fields should match with the object structure of @RequestBody on sb
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest request = new JsonObjectRequest(
+                Request.Method.POST,
+                SERVER_URL,
+                postBody,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        //  tvResponse.setText(response.toString());
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //tvResponse.setText(error.getMessage());
+                    }
+                }
+        ){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                HashMap<String, String> headers = new HashMap<String, String>();
+                //                headers.put("Authorization", "Bearer YOUR_ACCESS_TOKEN");
+                //                headers.put("Content-Type", "application/json");
+                return headers;
+            }
+
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                //                params.put("param1", "value1");
+                //                params.put("param2", "value2");
+                return params;
+            }
+        };
+
+        // Adding request to request queue
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(request);
+    }
 
 }
