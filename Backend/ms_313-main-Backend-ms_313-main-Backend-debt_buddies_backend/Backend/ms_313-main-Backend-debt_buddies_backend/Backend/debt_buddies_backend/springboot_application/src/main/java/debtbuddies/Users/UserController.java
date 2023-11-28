e693cc5476c053ff9e3b0ea54e0d882cecabacc4
Owen Parker <owenparker@Owens-MacBook-Pro.local>
2023-11-28 15:19:50 -0600

    added new table objects

---
+++ Backend/ms_313-main-Backend-ms_313-main-Backend-debt_buddies_backend/Backend/ms_313-main-Backend-debt_buddies_backend/Backend/debt_buddies_backend/springboot_application/src/main/java/debtbuddies/Users/UserController.java
@@ -0,0 +1,103 @@
+package debtbuddies.Users;
+
+import java.util.List;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Vivek Bengre
+ *
+ */
+
+@Api(value = "UserController", description = "Rest Api used for user object")
+@RestController
+public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
+
+
+    private String success = "{\"message\":\"success\"}";
+    private String failure = "{\"message\":\"failure\"}";
+
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success|OK"),
+            @ApiResponse(code = 401, message = "not authorized!"),
+            @ApiResponse(code = 403, message = "forbidden!!!"),
+            @ApiResponse(code = 404, message = "not found!!!") })
+    @GetMapping(path = "/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success|OK"),
+            @ApiResponse(code = 401, message = "not authorized!"),
+            @ApiResponse(code = 403, message = "forbidden!!!"),
+            @ApiResponse(code = 404, message = "not found!!!") })
+    @GetMapping(path = "/users/{id}")
+    User getUserById( @PathVariable int id){
+        return userRepository.findById(id);
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success|OK"),
+            @ApiResponse(code = 401, message = "not authorized!"),
+            @ApiResponse(code = 403, message = "forbidden!!!"),
+            @ApiResponse(code = 404, message = "not found!!!") })
+    @PostMapping(path = "/users")
+    String createUser(@RequestBody User user){
+        if (user == null)
+            return failure;
+        userRepository.save(user);
+        return success;
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success|OK"),
+            @ApiResponse(code = 401, message = "not authorized!"),
+            @ApiResponse(code = 403, message = "forbidden!!!"),
+            @ApiResponse(code = 404, message = "not found!!!") })
+    @PutMapping("/users/{id}")
+    User updateUser(@PathVariable int id, @RequestBody User request){
+        User user = userRepository.findById(id);
+        if(user == null)
+            return null;
+        userRepository.save(request);
+        return userRepository.findById(id);
+    }
+
+    /*@PutMapping("/users/{userId}/laptops/{laptopId}")
+    String assignLaptopToUser(@PathVariable int userId,@PathVariable int laptopId){
+        User user = userRepository.findById(userId);
+        Laptop laptop = laptopRepository.findById(laptopId);
+        if(user == null || laptop == null)
+            return failure;
+        laptop.setUser(user);
+        user.setLaptop(laptop);
+        userRepository.save(user);
+        return success;
+    }*/
+
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success|OK"),
+            @ApiResponse(code = 401, message = "not authorized!"),
+            @ApiResponse(code = 403, message = "forbidden!!!"),
+            @ApiResponse(code = 404, message = "not found!!!") })
+    @DeleteMapping(path = "/users/{id}")
+    String deleteUser(@PathVariable int id){
+        userRepository.deleteById(id);
+        return success;
+    }
+}
