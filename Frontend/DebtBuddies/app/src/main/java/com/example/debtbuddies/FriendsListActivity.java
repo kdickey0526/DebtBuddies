165eadafaede29ca4f58b13ff51763d0e4da806f
kdickey <kdickey@iastate.edu>
2023-12-05 16:18:03 -0600

    more work on friends list and leaderboard

--- Frontend/DebtBuddies/app/src/main/java/com/example/debtbuddies/FriendsListActivity.java
+++ Frontend/DebtBuddies/app/src/main/java/com/example/debtbuddies/FriendsListActivity.java
@@ -11,9 +11,12 @@ import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
+import com.android.volley.Response.ErrorListener;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 /**
@@ -24,8 +27,9 @@ public class FriendsListActivity extends AppCompatActivity {
 
     private TextView listOfFriends;
     private String SERVER_URL = "http://coms-309-048.class.las.iastate.edu:8080/person/";
-    private JSONObject userFriendList = null;
+    private JSONArray userFriendList = null;
     private JSONObject currentFriend = null;
+    private int friendID;
     private static final String TAG = "FriendsListActivity";
 
     // =============================================================================================================================================
@@ -47,10 +51,10 @@ public class FriendsListActivity extends AppCompatActivity {
 
         if (!MyApplication.loggedInAsGuest) {
             // setup server URL
-            SERVER_URL += MyApplication.currentUserName + "/friends"; // or something similar, however url needs to change
+            SERVER_URL += "getAllFriends/" + MyApplication.currentUserName; // or something similar
 
             // request user's friends list from backend here
-            makeJsonObjReq();
+            makeJsonArrayReq();
 
             // running theory is that receives json object of ID's of each friend, so grab each friend and append data
             // for each loop for each friend in the list/json object
@@ -60,21 +64,25 @@ public class FriendsListActivity extends AppCompatActivity {
             try {
                 int i = 0;
                 for (i = 0; i < userFriendList.length(); i++) { // .length returns # of key/value pairs in the object
-                    int friendID = userFriendList.getInt("friend" + i); // not sure how the friends are mapped in the database, should grab the friend's ID though
+                    friendID = userFriendList.getJSONObject(i).getInt("person_id"); // getInt("person_id" + i); // not sure how the friends are mapped in the database, should grab the friend's ID though
+
                     SERVER_URL = "http://coms-309-048.class.las.iastate.edu:8080/person/" + friendID;
                     makeFriendJsonObjReq(); // get that friend's info
-                    boolean onlineStatus = currentFriend.getBoolean("online"); // not sure what it's actually named in backend
+
+                    boolean onlineStatus = currentFriend.getBoolean("is_online"); // not sure what it's actually named in backend
+                    String name = currentFriend.getString("name");
                     String status = "";
+
                     if (onlineStatus)
                         status = "Online";
                     else
                         status = "Offline";
 
                     String previousList = listOfFriends.getText().toString();
-                    listOfFriends.setText(previousList + currentFriend.getString("name") + "\t\tStatus: " + status + "\n");
+                    listOfFriends.setText(previousList + name + "\t\tStatus: " + status + "\n");
                 }
             } catch (Exception e) {
-                Log.e(TAG, "The userFriendList probably returned null.");
+                Log.e(TAG, "The userFriendList JSONObject is probably null.");
                 e.printStackTrace();
                 Log.d(TAG, "userFriendList value = " + userFriendList);
             }
@@ -84,32 +92,28 @@ public class FriendsListActivity extends AppCompatActivity {
     }
 
     /**
-     * Makes a json object request with the given user information.
+     * Makes a json array request with the given user information.
      * Updates some fields on the screen for debugging purposes as well.
      **/
-    private void makeJsonObjReq() {
-        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET, SERVER_URL, null, new Response.Listener<JSONObject>() {
-            /**
-             * Updates some text views and instance variables according to the response.
-             * @param response
-             */
+    private void makeJsonArrayReq() {
+        JsonArrayRequest jsonArrReq = new JsonArrayRequest(Request.Method.GET, SERVER_URL, null, new Response.Listener<JSONArray>() {
             @Override
-            public void onResponse(JSONObject response) {
+            public void onResponse(JSONArray response) {
                 Log.d("Volley Response", "response received: " + response.toString());
-                userFriendList = response; // store json object
+                try {
+                    userFriendList = response; //new JSONArray("{\n\t\"Array\": " + response + "\n}"); // store json object
+                } catch (Exception e) {
+                    Log.e(TAG, "Failed ");
+                }
             }
-        }, new Response.ErrorListener() {
-            /**
-             * Displays the error to Logcat.
-             * @param error
-             */
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("Volley Error", error.toString());
-            }
-        });
-
-        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjReq);
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("Volley Error", error.toString());
+                    error.printStackTrace();
+                }
+            });
+            VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonArrReq);
     }
 
     /**
@@ -127,7 +131,7 @@ public class FriendsListActivity extends AppCompatActivity {
                 Log.d("Volley Response", "response received: " + response.toString());
                 currentFriend = response; // store json object
             }
-        }, new Response.ErrorListener() {
+        }, new ErrorListener() {
             /**
              * Displays the error to Logcat.
              * @param error
--- Frontend/DebtBuddies/app/src/main/java/com/example/debtbuddies/LeaderboardActivity.java
+++ Frontend/DebtBuddies/app/src/main/java/com/example/debtbuddies/LeaderboardActivity.java
@@ -3,12 +3,130 @@ package com.example.debtbuddies;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class LeaderboardActivity extends AppCompatActivity {
+
+    private static final String TAG = "LeaderboardActivity";
+    private String SERVER_URL = "http://coms-309-048.class.las.iastate.edu:8080/person/Whackamole"; //person/WhacAMole for old version
+    private TextView tv_whacBoard;
+    private TextView tv_warBoard;
+    private TextView tv_bjBoard;
+
+    private JSONArray leaderboardArray; // will be re-assigned to update each textview
+    private JSONObject currentPerson; // holds current object/person being added to whichever leaderboard
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
+
+        // instantiate views
+        tv_whacBoard = findViewById(R.id.tv_whacLeaderboard);
+        tv_warBoard = findViewById(R.id.tv_warLeaderboard);
+        tv_bjBoard = findViewById(R.id.tv_blackjackLeaderboard);
+
+        updateWhac();
+//        updateBJ();
+//        updateWar();
     }
+
+    private void updateWhac() {
+        SERVER_URL = "http://coms-309-048.class.las.iastate.edu:8080/person/Whackamole"; // will have to be updated after basic testing
+        makeJsonArrayReq();
+        int i;
+        for (i = 0; i < leaderboardArray.length(); i++) { // should only give 5 objects based on size in backend
+            try {
+                currentPerson = leaderboardArray.getJSONObject(i);
+                String name = currentPerson.getString("name");
+                String s = tv_whacBoard.getText().toString();
+                tv_whacBoard.setText(s + (i+1) + ": " + name + "\n");
+
+            } catch (JSONException e) {
+                Log.e(TAG, "Failed getting the JSONObject or elements of it within the array");
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
+
+    private void updateWar() {
+
+    }
+
+    private void updateBJ() {
+
+    }
+
+
+    /**
+     * Makes a json array request with the given user information.
+     * Updates some fields on the screen for debugging purposes as well.
+     **/
+    private void makeJsonArrayReq() {
+        JsonArrayRequest jsonArrReq = new JsonArrayRequest(Request.Method.GET, "http://coms-309-048.class.las.iastate.edu:8080/person/Whackamole", null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                Log.d("Volley Response", "response received: " + response.toString());
+                try {
+                    leaderboardArray = response; //new JSONArray("{\n\t\"Array\": " + response + "\n}"); // store json object
+                } catch (Exception e) {
+                    Log.e(TAG, "Failed setting leaderboardArray");
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Volley Error -- Array not received", error.toString());
+                error.printStackTrace();
+            }
+        });
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonArrReq);
+    }
+
+    /**
+     * Makes a json object request with the given user information.
+     * Updates some fields on the screen for debugging purposes as well.
+     **/
+    private void makeJsonObjReq() {
+        JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET, SERVER_URL, null, new Response.Listener<JSONObject>() {
+            /**
+             * Updates some text views and instance variables according to the response.
+             * @param response
+             */
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d("Volley Response", "response received: " + response.toString());
+//                try {
+//
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+            }
+        }, new Response.ErrorListener() {
+            /**
+             * Displays the error to Logcat.
+             * @param error
+             */
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Volley Error", error.toString());
+            }
+        });
+
+        VolleySingleton.getInstance(getApplicationContext()).addToRequestQueue(jsonObjReq);
+    }
+
 }
